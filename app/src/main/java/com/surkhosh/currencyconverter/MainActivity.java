3835366b248f096598380f16acd7f50e4fc6c367
srsh-kh <sur.khosh@gmail.com>
2022-12-30 20:14:47 +0330

    x

--- app/src/main/java/com/surkhosh/currencyconverter/MainActivity.java
+++ app/src/main/java/com/surkhosh/currencyconverter/MainActivity.java
@@ -3,9 +3,22 @@ package com.surkhosh.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public void convertCurrency(View view){
+        Log.i("Info", "Button pressed");
+        EditText editText=(EditText) findViewById(R.id.editText);
+        String amountInPounds=editText.getText().toString();
+        double amountInPoundsDouble=Double.parseDouble(amountInPounds);
+        double amountInDollarsDouble=amountInPoundsDouble*1.3;
+        String amountInDollarsString=String.format("%.2f", amountInDollarsDouble);
+        Toast.makeText(this, "£"+amountInPounds+" is $"+amountInDollarsDouble, Toast.LENGTH_SHORT).show();
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
